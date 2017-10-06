863c3c849fcc851d0b0d809f9cc68b99a77b434b
Alexander Beskine <a.d.beskine@outlook.com>
2017-10-06 16:58:24 +0100

    initial testing wireframe for facebook stream mechanism

--- dashboard/facebook_data/tests.py
+++ dashboard/facebook_data/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
+class facebook_authentication_tests(TestCase):
+
+	def test_login_flow(self):
+		# using a valid facebook login flow a user can generate an access token to be stored
+
+	def test_hostile_command(self):
+		# if a command which is NOT from the dashboard is received by the facebook_data app it is rejected and reported to the account owner.
+
+
+
+class facebook_post_interaction_tests(TestCase):
+
+	def test_post_comment_command_from_dashboard(self):
+		# when facebook_data app receives post_comment command from dashboard it successfully posts the comment in the right place
+
+	def test_like_object_command_from_dashboard(self):
+		# when facebook_data app receives like_comment command from dashboard it successfully likes the correct object
+
+	def test_unlike_object_command_from_dashboard(self):
+		# when facebook_data app receives unlike_comment command from dashboard it successfully unlikes the correct object
+
+
+
+class facebook_CRUD_tests(TestCase):
+
+	def test_previous_posts_command_from_dashboard(self):
+		# when facebook_data_app receives previous_posts command it successfully receives AND feeds back the previous posts
+
+	def test_new_post_command_from_dashboard(self):
+		# when facebook_data app receives new_post command it successfully posts a new post
+
+	def test_delete_object_command_from_dashboard(self):
+		# when facebook_data app receives delete_object command it successfully deletes the new post
+
+
+class facebook_statistics_test(TestCase):
+
+
+# TODO - tests for failing gracefully
+
 # Create your tests here.
--- dashboard/render_dashboard/tests.py
+++ dashboard/render_dashboard/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
+class dashboard_config_options(TestCase):
+
+	def test_add_facebook_stream(self):
+		# a user can successfully autheticate with facebook, connect to a page and have the page data begin rendering on the dashboard
+
+
+class dashboard_commands_facebook(TestCase):
+
+	def test_fetch_feed(self):
+		# when a stream is loaded it correctly fetches the feed of the page it's linked to
+
+	def test_new_post(self):
+		# when a new_post command is sent from the dashboard a new post is posted, the page refreshes and the new post appears correctly
+
+	def test_delete_object(self):
+		# when a delete_object command is sent from the dashboard the object is deleted from facebook.
+		# note: the delete object option will not be available to a user on an object he does not have permission to delete
+
+	def test_post_comment(self):
+		# when a post_comment command is sent from the dashboard the comment is posted, the dashboard refreshes and the comment appears correctly
+
+	def test_like_object(self):
+		# when a like_object command is sent from the dashboard the object is liked, the dashboard refreshes and the like appears correctly
+
+	def test_unlike_object(self):
+		# when an unlike_object command is sent from the dashboard the object is unliked, the dashboard refreshes and the object correctly appears unliked
+
+# TODO - tests for failing gracefully
+
+
 # Create your tests here.
